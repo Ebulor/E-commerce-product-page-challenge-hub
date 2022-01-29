6dbb2c4395d7086f8726a8c3274756463113d996
Adaeze Dawari <78680916+Ebulor@users.noreply.github.com>
2022-01-29 13:55:11 +0400

    final

--- nav.js
+++ nav.js
@@ -1,11 +1,14 @@
 const openNav = document.querySelector(".toggle-menu");
 const closeNav = document.querySelector(".close-menu");
 const nav = document.querySelector(".navbar");
+const body = document.querySelector("body");
 
 openNav.addEventListener("click", () => {
   nav.classList.add("nav-active");
+  body.classList.add("nav-open");
 });
 
 closeNav.addEventListener("click", () => {
   nav.classList.remove("nav-active");
+  body.classList.remove("nav-open");
 });
